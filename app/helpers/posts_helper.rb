c8faa14fd08d9db091d5b584e6f135f575390831
tadejrebernjak <tadej.rebernjak2@gmail.com>
2021-11-16 12:56:50 +0100

    helper fix

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,20 +2,18 @@ module PostsHelper
     def display_index_post_links(post)
         capture do
             concat link_to('Preberi več', post, class: 'stretched-link')
-            if current_user == post.user
-                concat link_to('Uredi', edit_post_path(post))
-                concat link_to('Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' })
-            end
         end
     end
 
     def display_show_post_links(post)
         capture do
             if current_user == post.user
-                concat link_to 'Edit', edit_post_path(@post)
+                concat link_to 'Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' }
+                concat ' | '
+                concat link_to 'Uredi', edit_post_path(@post)
                 concat ' | '
             end
-            concat link_to 'Back', posts_path
+            concat link_to 'Nazaj', posts_path
         end
     end
 end
