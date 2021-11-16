7eb670f05189ce3743305678328da37eb627b51e
tadejrebernjak <tadej.rebernjak2@gmail.com>
2021-11-16 12:14:36 +0100

    dodal bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,12 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+// CSS
+import 'scss/site'
+// JS
+import('js/site')
+// Images
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
